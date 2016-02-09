85bcd0b56d2e74c61843df2d7ac3e37690350e62
Vallery Lancey <vallerylancey@gmail.com>
2016-02-08 19:17:40 -0800

    Devised a struction for /nolphins, did some basic work on populating nolphin webpages

--- routes/generations.js
+++ routes/generations.js
@@ -1,21 +1,56 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
 var router = express.Router();
 
 
 
 router.get('/', function(req, res) {
-	res.render('generations', {
-		title: 'Generations',
-		activePage: 'generations'
-	});
+
+	var doneFunc = 	function(generations) {
+		console.log(generations);
+		res.render('generations', {
+			title: 'Generations',
+			activePage: 'generations',
+			'generations': generations
+		});
+	}
+
+	var generations = {};
+	var gens = 5;
+	
+	function readGenInfo(i) {
+		fs.readFile(path.join(__dirname, '../nolphins/'+i+'/info.json'), 'utf8', function(err, info) {
+			if (err) {
+				console.log(err);
+			}
+			generations[i] = {
+				'gen' : i,
+				'info' : JSON.parse(info)
+			}
+			if (i > 1) {
+				readGenInfo(i-1, doneFunc);
+			}
+			else {
+				doneFunc(generations);
+			}
+		});
+	}
+
+	readGenInfo(gens);
 });
 
 
 router.get('/:generation', function(req, res) {
+	var info = require('../nolphins/'+req.params.generation+'/info');
 	res.render('nolphin-list', {
 		title: 'v'+req.params.generation,
 		activePage: 'generations',
-		generation: req.params.generation
+		generation: req.params.generation,
+		name: info.name,
+		name_full: info.name_full,
+		description: info.description
 	});
 });
 
