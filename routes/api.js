aa012b62cdf5e74ef1e94c3bd49100befbd8d4e1
Vallery Lancey <vallerylancey@gmail.com>
2016-02-13 11:27:12 -0800

    Added an API route for downloading "the newest nolphin"

--- routes/api.js
+++ routes/api.js
@@ -14,21 +14,58 @@ router.get('/', function(req, res) {
 
 
 
-router.get('/download/5', function(req, res) {
-	var dir = path.join(__dirname, '../nolphins/5')
-	fs.readdir(dir, function(err, items) {
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
+}
+
+function getFiles(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isFile();
+  });
+}
+
+//I tried to async this and Gave Up
+var findSuitableNolphin = function(genDir, downloadType, devVersion, funcOk) {
+	arrayOfModels = getDirectories(genDir);
+	for (var i=arrayOfModels.length-1; i>=0; i--) {
+		var info = JSON.parse(fs.readFileSync(path.join(genDir, arrayOfModels[i]+'/info.json'), "utf8"));
+		if (!info.main) { //Only look at the "main line" of Nolphins, not special derivatives.
+			continue;
+		}
+		arrayOfSubversions = getFiles(path.join(genDir, arrayOfModels[i]+'/'+downloadType));
+		for (var j=arrayOfSubversions.length-1; j>=0; j--) {
+			if(devVersion || (arrayOfSubversions[j].indexOf('alpha') == -1 && arrayOfSubversions[j].indexOf('beta') == -1)) { //Only return alphas/betas if specified.
+				funcOk({
+					'success': true,
+					'message': 'Nolphin downloaded',
+					'name': arrayOfSubversions[j],
+					'dataType': downloadType,
+					'data': fs.readFileSync(path.join(genDir, arrayOfModels[i]+'/'+downloadType+'/'+arrayOfSubversions[j]), "utf8")
+				});
+				return;
+			}
+		}
+	}
+	funcOk({'success':false, 'message':'No Nolphin :('});
+
+}
+
+router.get('/download/:generation/:downloadType', function(req, res) {
+	var generation = req.params.generation;
+	var downloadType = req.params.downloadType;
+	var dir = path.join(__dirname, '../nolphins/' + generation) + '/'
+
+	fs.readdir(dir, function(err, models) {
 		if (err) {
-			console.log(err);
 			res.json({'success':false, 'err':err})
 			return;
 		}
-		
-		for (var i=items.length-1; i>=0; i--) {
-			if (fs.lstatSync(path.join(dir, items[i])).isDirectory()) {
-				console.log(items[i]);
-			}
-		}
-		res.json({'success':true});
+
+		findSuitableNolphin(dir, downloadType, req.query.dev, function(nolphin) {
+			res.json(nolphin);
+		});
 	});
 });
 
